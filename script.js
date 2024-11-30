c729aa638fd03be3ec43b3e88df41961ca44d559
Gnanamalarupr <gnanamalarpalanisamy@gmail.com>
2024-11-30 21:37:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const display = document.querySelector('.display span');
+const buttons = document.querySelectorAll('.calc');
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.textContent;
+
+   
+    if (value === 'AC') {
+      display.textContent = '0';
+    }
+    
+    else if (value === 'DEL') {
+      display.textContent = display.textContent.slice(0, -1);
+      if (display.textContent === '') {
+        display.textContent = '0';
+      }
+    } 
+    else if (value === '=') {
+      try {
+        display.textContent = eval(display.textContent);
+      } catch (error) {
+        display.textContent = 'Error';
+      }
+    }
+   
+    else {
+      if (display.textContent === '0') {
+        display.textContent = value;
+      } else {
+        display.textContent += value;
+      }
+    }
+  });
+});
